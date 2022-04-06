3f8b319f446545e1d7065e65d7e91509789ce7f0
Cyanosite <zsomborszenyan@gmail.com>
2022-04-06 15:32:09 +0200

    test

--- bigint_benchmark.cpp
+++ bigint_benchmark.cpp
@@ -3,7 +3,7 @@
 #include "bigint.h"
 int main()
 {
-    Bigint<2048> asd("FFFFFFFFFFFFFFFF");
+    Bigint<2048> asd("123456789ABCDEF123456789ABCDEF");
     Bigint<2048> asd2(asd);
     Bigint<2048> asd3(asd);
     asd = asd + asd2 + asd3;
