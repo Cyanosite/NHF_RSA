15b95f02c934929da653306693ea5a12b5fec331
Cyanosite <zsomborszenyan@gmail.com>
2022-04-08 23:11:27 +0200

    added multiplication

--- bigint.h
+++ bigint.h
@@ -9,8 +9,9 @@ struct Bigint
     Bigint(const char *const &);
     Bigint(const Bigint &);
     ~Bigint();
-    void print() const;
     Bigint &operator=(const Bigint &);
+    bool operator==(const Bigint &) const;
+    bool operator!=(const Bigint &) const;
     unsigned int &operator[](const unsigned int &) const;
     Bigint operator+(const Bigint &) const;
     Bigint operator-(const Bigint &) const;
@@ -37,14 +38,14 @@ Bigint<bits>::Bigint(const char *const &x)
         int i = 0;
         for (; i < number_of_runs - 1; ++i)
         {
-            sscanf(startpos - i * 8, "%8llX", &storage[i]);
+            sscanf(startpos - i * 8, "%8X", &storage[i]);
         }
         char read_this[7] = "%";
-        sprintf(read_this + 1 * sizeof(char), "%dllX", (int)strlen(x) % 8);
+        sprintf(read_this + 1 * sizeof(char), "%dX", (int)strlen(x) % 8);
         sscanf(x, read_this, &storage[i]);
     }
     else
-        sscanf(x, "%llX", &storage[0]);
+        sscanf(x, "%X", &storage[0]);
 }
 template <unsigned int bits>
 Bigint<bits>::Bigint(const Bigint &x)
@@ -58,16 +59,28 @@ Bigint<bits>::~Bigint()
     delete[] storage;
 }
 template <unsigned int bits>
-void Bigint<bits>::print() const
+std::ostream &operator<<(std::ostream &os, const Bigint<bits> &x)
 {
-    unsigned short size = bits / sizeof(unsigned int);
-    std::cout << std::hex;
+    unsigned short size = bits / (sizeof(unsigned int) * 8);
+    os << std::hex;
+    bool isnull = true;
     for (int i = size - 1; i >= 0; --i)
     {
-        if (storage[i] != 0)
-            std::cout << storage[i];
+        if (isnull)
+        {
+            if (x[i] != 0)
+            {
+                os << x[i];
+                isnull = false;
+            }
+        }
+        else
+            os << x[i];
     }
-    std::cout << std::dec << std::endl;
+    if (isnull)
+        os << 0;
+    os << std::dec;
+    return os;
 }
 template <unsigned int bits>
 Bigint<bits> &Bigint<bits>::operator=(const Bigint &x)
@@ -76,6 +89,26 @@ Bigint<bits> &Bigint<bits>::operator=(const Bigint &x)
     storage = new unsigned int[bits / sizeof(unsigned int)]{0};
     std::memcpy(storage, x.storage, sizeof(x.storage) * sizeof(unsigned int));
     return *this;
+}
+template <unsigned int bits>
+bool Bigint<bits>::operator==(const Bigint &x) const
+{
+    for (unsigned int i = 0; i < bits / (sizeof(unsigned int) * 8); ++i)
+    {
+        if (storage[i] != x.storage[i])
+            return false;
+    }
+    return true;
+}
+template <unsigned int bits>
+bool Bigint<bits>::operator!=(const Bigint &x) const
+{
+    for (unsigned int i = 0; i < bits / (sizeof(unsigned int) * 8); ++i)
+    {
+        if (storage[i] != x.storage[i])
+            return true;
+    }
+    return false;
 }
 template <unsigned int bits>
 unsigned int &Bigint<bits>::operator[](const unsigned int &i) const
@@ -88,7 +121,7 @@ Bigint<bits> Bigint<bits>::operator+(const Bigint &x) const
     Bigint res(x);
     unsigned short carry = 0;
     unsigned long long temp;
-    for (unsigned int i = 0; i < bits / 64; ++i)
+    for (unsigned int i = 0; i < bits / (sizeof(unsigned int) * 8); ++i)
     {
         temp = (unsigned long long)res[i] + (unsigned long long)storage[i] + (unsigned long long)carry;
         carry = temp >> (8 * sizeof(unsigned int));
@@ -99,8 +132,41 @@ Bigint<bits> Bigint<bits>::operator+(const Bigint &x) const
     return res;
 }
 template <unsigned int bits>
-Bigint<bits> Bigint<bits>::operator*(const Bigint &) const
+Bigint<bits> Bigint<bits>::operator-(const Bigint &x) const
 {
+    Bigint res(x);
+    unsigned short borrow = 0;
+    unsigned long long temp;
+    for (unsigned int i = 0; i < bits / (sizeof(unsigned int) * 8); ++i)
+    {
+        temp = (unsigned long long)res[i] - ((unsigned long long)storage[i] + (unsigned long long)borrow);
+        borrow = temp >> (8 * sizeof(unsigned int));
+        res[i] = (unsigned int)temp;
+    }
+    if (borrow)
+        throw("underflow");
+    return res;
 }
-
+template <unsigned int bits>
+Bigint<bits> Bigint<bits>::operator*(const Bigint &x) const
+{
+    Bigint res;
+    unsigned short carry = 0;
+    unsigned short k = 0;
+    unsigned long long temp;
+    for (unsigned int i = 0; i < bits / (sizeof(unsigned int) * 8); ++i)
+    {
+        for (unsigned int j = 0; j < bits / (sizeof(unsigned int) * 8); ++j)
+        {
+            k = i + j;
+            temp = (unsigned long long)storage[i] * (unsigned long long)x.storage[j] + (unsigned long long)carry;
+            carry = temp >> (8 * sizeof(unsigned int));
+            res[k] += (unsigned int)temp;
+        }
+        res[k] = carry;
+    }
+    if (carry)
+        throw("overflow");
+    return res;
+}
 #endif
--- bigint_benchmark.cpp
+++ bigint_benchmark.cpp
@@ -1,12 +1,19 @@
 #include <ctime>
+#include <iostream>
 #include "memtrace.h"
 #include "bigint.h"
 int main()
 {
-    Bigint<2048> asd("123456789ABCDEF123456789ABCDEF");
-    Bigint<2048> asd2(asd);
-    Bigint<2048> asd3(asd);
-    asd = asd + asd2 + asd3;
-    asd.print();
+    Bigint<2048> bignumber("123456789ABCDEF123456789ABCDEF");
+    Bigint<2048> big_copy(bignumber);
+    Bigint<2048> big_copy2(bignumber);
+    bignumber = bignumber + big_copy + big_copy2 + big_copy;
+    std::cout << bignumber << std::endl;
+    Bigint<2048> sub = bignumber - bignumber;
+    std::cout << sub << std::endl;
+    Bigint<2048> mul = big_copy * Bigint<2048>(4);
+    std::cout << mul << std::endl;
+    if (bignumber == mul)
+        std::cout << "bignumber and mul are equal" << std::endl;
     return 0;
 }
